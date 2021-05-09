2a3b3ead12da74a2da696441dbe3657a8071d320
Huy Nguyen <mrvietnamese.ca@gmail.com>
2021-05-09 14:13:53 -0400

    added dashboard events

--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -1,9 +1,44 @@
 import Layout from '@/components/Layout'
 
-export default function DashboardPage() {
+import DashboardEvent from '@/components/DashboardEvent'
+
+import { parseCookies } from '@/helpers/index'
+import { API_URL } from '@/config/index'
+
+import styles from '@/styles/Dashboard.module.css'
+
+export default function DashboardPage({ events }) {
     return (
         <Layout title='User Dashboard'>
-            <h1>dashboard </h1>
+            <div className={styles.dash}>
+                <h1>dashboard </h1>
+                <h3>My events</h3>
+
+                {events.map((event) => (
+                    <DashboardEvent key={event.id} evt={event} />
+                ))}
+
+            </div>
         </Layout>
     )
+}
+
+
+export async function getServerSideProps({ req }) {
+    const { token } = parseCookies(req)
+
+    const res = await fetch(`${API_URL}/events/me`, {
+        method: 'GET',
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    })
+
+    const events = await res.json()
+
+    return {
+        props: {
+            events
+        }
+    }
 }
